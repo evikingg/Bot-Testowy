5a764c861d1e1a18e89572ebd92fdab688e405b5
evikingg <51865595+evikingg@users.noreply.github.com>
2021-11-16 01:32:26 +0100

    TofuMode
    Adds toffuMode(Beta). Nie działa zmiana nazwy kanału..

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const client = new Client({
     ]
 })
 
+const tofuID = '543879286554230784';
+
 var toggleToffuMode = 0;
 
 
@@ -25,28 +27,36 @@ client.on('interactionCreate', interaction => {
 
     if(commandName === 'toffumode') {
         if(!interaction.member.roles.cache.some(role => role.name === 'HEAD ADMIN')) {      // KOMENDA TYLKO DLA H@
+            interaction.channel.send('Nie masz do tego uprawnień do użycia ' + commandName + ' ' + interaction.member.user.tag + ' !');
             return;
         }
 
         if(toggleToffuMode === 0) {         // WŁĄCZ/WYŁĄCZ TOFFU MODE
             toggleToffuMode = 1;
             interaction.channel.send('ToffuMode został włączony');
+            interaction.guild.channels.cache.get('909904844523974667').setName('ToffuMode: ON');
         } else {
             toggleToffuMode = 0;
             interaction.channel.send('ToffuMode został wyłączony');
+            interaction.guild.channels.cache.get('909904844523974667').setName('ToffuMode: OFF');
         }
     }
 
     if(commandName === 'ping') {
         interaction.reply('pong');
     }
+    
+    if(commandName === 'serverinfo') {
+        interaction.reply('Nazwa: ' + interaction.guild.name + 'Ilość użytkowników: ' + interaction.guild.memberCount);
+    }
 })
 
+
 client.on('messageCreate', message => {
-    if(message.author.id === '298103131898773504' && toggleToffuMode === 1) {       // USUWANIE WIADOMOŚCI TOFA
+    if(message.author.id === tofuID && toggleToffuMode === 1) {       // USUWANIE WIADOMOŚCI TOFA
         message.delete({setTimeout: 100});
         message.channel.send('`:)`');
-       // client.channels.cache.get(`909893873935990784`).send('```tofu chyba próbuje coś napisać..```jakby to kogoś obchodziło, to napisał "' + message.content + '", aaa, napisał to na kanale:' + message.channel);
+        client.channels.cache.get(`909893873935990784`).send('```tofu chyba próbuje coś napisać..```jakby to kogoś obchodziło, to napisał "' + message.content + '", aaa, napisał to na kanale:' + message.channel);
     }
 
     if(message.channel.name === 'liczenie-znakow' && message.author.bot === false && message.author.id !== '543879286554230784') {
@@ -57,45 +67,4 @@ client.on('messageCreate', message => {
 })
 
 
-
-/*
-
-client.on('messageCreate', (message) => {
-    if (message.content.charAt(0) === '!') {
-        if (message.content.substring(1) === 'ping' && message.author.id !== '543879286554230784') { 
-     message.channel.send("pong!");
-        }
-    }
-
-    if(message.content !== '!ping' && message.channel.name === 'liczenie-znakow' && message.author.bot === false && message.author.id !== '543879286554230784') {
-        message.reply({
-            content: 'wiadomosc ma nastepujaca ilosc znakow: ' + message.content.length
-        })
-    }
-
-    if(message.author.id === '543879286554230784' && toggleToffuMode === 1) {
-        message.delete({setTimeout: 100});
-        message.channel.send('`:)`');
-        client.channels.cache.get(`909893873935990784`).send('```tofu chyba próbuje coś napisać..```jakby to kogoś obchodziło, to napisał "' + message.content + '"');
-    }
-
-    if(message.content === '!random') {
-        message.channel.send('Wylosowales/as: ' + Math.round(Math.random()*10));
-    }
-
-    if(message.member.roles.cache.some(role => role.name === 'HEAD ADMIN') && message.content === '!toffuMode') {
-        if(toggleToffuMode === 0) {
-            toggleToffuMode = 1;
-            message.channel.send('ToffuMode został włączony');
-        } else {
-            toggleToffuMode = 0;
-            message.channel.send('ToffuMode został wyłączony');
-        }
-    }
-
-})
-
-*/
-
-
-client.login('token');
+client.login('OTA4MzY2MTcyMTg4MTg4Njky.YY0r7A.Q9NQbsnP0ewC_np-LluMLBtNV9w');
